1725a606f3df8e2fccf7b279046af22cf7b946ff
Elina Tang <elina@localadmins-MacBook-Air.local>
2017-10-08 15:06:52 -0500

    This is commit

---
+++ ex2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Tester
+{
+ public static void main( String args[] )
+{
+ Scanner kbReader = new Scanner(System.in);
+ System.out.print(“What's your name?_ ”);
+ double name = kbReader.nextDouble( );
+ System.out.println("Hello,"+ name );
+}
+} 
